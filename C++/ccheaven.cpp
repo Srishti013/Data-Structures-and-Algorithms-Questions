06e6560fb30bcc839a7a6c4339f6d25bacbc9a59
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-05-01 12:20:15 +0530

    Create ccheaven.cpp

---
+++ C++/ccheaven.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int l;
+	    cin>>l;
+	    string s;
+	    cin>>s;
+	    float c=0,lo=0;
+	    bool flag=0;
+	    for(int i=0;i<l;i++)
+	    {
+	        lo++;
+	        if(s[i]=='1')
+	        c++;
+	        if((lo/2)<=c)
+	        {
+	            cout<<"YES\n";
+	            flag=1;
+	            break;
+	        }
+	        
+	    }
+	    if(!flag)
+	    cout<<"NO\n";
+	}
+	return 0;
+}
