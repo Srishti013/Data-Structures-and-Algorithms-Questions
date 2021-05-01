d2d7cf805a2a230e1456ee5d673bc84bc7bd7045
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-05-01 12:23:55 +0530

    Create coldplay.cpp

---
+++ C++/coldplay.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m,s;
+	    cin>>m>>s;
+	    int c;
+	    c=m/s;
+	    //if(m<s)
+	    //cout<<0;
+	    //else
+	    cout<<c<<"\n";
+	}
+	return 0;
+}
