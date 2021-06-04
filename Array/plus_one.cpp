bbaed7ef925cf85afe15179a2be99c83b7eaee08
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-06-04 14:50:35 +0530

    Create plus_one.cpp

---
+++ Array/plus_one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            
+            if(digits[i]==9 && i==0)
+            {
+                digits[i]=0;
+                digits.insert(digits.begin(),1);
+                
+             }
+            else if(digits[i]!=9)
+            {
+                digits[i]+=1;
+                break;
+            }
+            else if(digits[i]==9)
+            {
+                digits[i]=0;
+             }
+         }
+         
+        return digits;
+    }
+};
