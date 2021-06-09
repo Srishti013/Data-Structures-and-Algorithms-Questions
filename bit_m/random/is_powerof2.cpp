d306485b4da64e06ea176019b747a1f637a1965b
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-06-09 14:15:17 +0530

    Create is_powerof2.cpp

---
+++ bit_m/random/is_powerof2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power2(int n)
+{
+    return (n && !(n&(n-1)));
+}
+int main() 
+{
+    int n;
+    cin>>n;
+    cout<<power2(n);
+	return 0;
+}
