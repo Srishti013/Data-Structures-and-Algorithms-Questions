4da6455f135af3bc3f603e5f2f0842a44c60be98
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-10-14 11:54:41 +0530

    Update insert_in_sorted.cpp

--- linked_list/insert_in_sorted.cpp
+++ linked_list/insert_in_sorted.cpp
@@ -1,25 +1,26 @@
-Node * insertInSorted(Node * head, int data)
-{
-    Node *curr=head;
-    Node *new_node=new Node(data);
-    if(head->data > data)
-    {
-        new_node->next=head;
-        return new_node;
-    }
-    while(curr->next!=NULL && (curr->next->data)<=data)
-    {
-        curr=curr->next;
-    }
-    if(curr->next==NULL)
-    {
-        curr->next=new_node;
-    }
-    else
-    {
-        new_node->next=curr->next;
-        curr->next=new_node;
-    }
-    return head;
+class Solution{
+  public:
     
-}
+    Node *sortedInsert(struct Node* head, int data) {
+        Node *new_node=new Node(data);
+        Node *curr=head;
+        if(head->data > data)
+        {
+            new_node->next=head;
+            return new_node;
+        }
+        while(curr->next!=NULL)
+        {
+            if(curr->next->data>data)
+            {
+                new_node->next=curr->next;
+                curr->next=new_node;
+                return head;
+            }
+            curr=curr->next;
+        }
+        curr->next=new_node;
+        return head;
+        
+    }
+};
