ff96459ad727b170d501a15dce2d6a585687cd82
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-10-24 20:37:21 +0530

    Create sum_left_leaves.cpp

---
+++ tree/sum_left_leaves.cpp
@@ -0,0 +1,19 @@
+int leftLeavesSum(Node *root)
+{
+    
+    if(root==NULL)
+    return 0;
+    
+    if(root->left==NULL && root->right==NULL)
+    return 0;
+    
+    if(root->left!=NULL)
+    {
+    if(root->left->left==NULL && root->left->right==NULL)
+    return root->left->data +leftLeavesSum(root->right);
+    }
+    
+    return leftLeavesSum(root->left) + leftLeavesSum(root->right);
+    
+    
+}
