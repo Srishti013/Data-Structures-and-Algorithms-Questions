c77bc9f3d18b3247a5a7bb924dedfd7c41c2b4a1
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2022-02-15 13:53:24 +0530

    Update find_dup.cpp

--- Array/find_dup.cpp
+++ Array/find_dup.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 vector<int> duplicates(int arr[], int n) {
         vector<int> a;
         sort(arr,arr+n);
@@ -26,3 +27,34 @@ vector<int> duplicates(int arr[], int n) {
         else
         return a;
     }
+
+
+// Solution 2
+
+
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        vector<int> ans;
+        int x[n] = {0};
+        for(int i=0;i<n;i++)
+        {
+            if(x[arr[i]]>0)
+            {
+                ans.push_back(arr[i]);
+                x[arr[i]]=-1;
+            }
+            else if(x[arr[i]]==0)
+            {
+                x[arr[i]]++;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        if(ans.empty())
+        {
+            ans.push_back(-1);
+        }
+        return ans;
+       
+    }
+};
