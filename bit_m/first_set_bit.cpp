d3da20503287f9b511bea6e07e57729043a363ae
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2022-04-18 12:42:45 +0530

    Update first_set_bit.cpp

--- bit_m/first_set_bit.cpp
+++ bit_m/first_set_bit.cpp
@@ -1,37 +1,17 @@
-//Initial Template for C++
-
-
-#include<bits/stdc++.h>
-using namespace std;
-
-
- // } Driver Code Ends
-//User function Template for C++
-
 class Solution
 {
     public:
-    //Function to find position of first set bit in the given number.
+    
     unsigned int getFirstSetBit(int n)
     {
-        return ffs(n);
+        int res=0;
+        while(n>0)
+        {
+            res++;
+            if(n%2!=0)
+                break;
+            n/=2;
+        }
+        return res;
     }
 };
-
-// { Driver Code Starts.
-
-// Driver code
-int main()
-{
-    int t;
-    cin>>t; // testcases
-    while(t--)
-    {
-        int n;
-        cin>>n; //input n
-        Solution ob;
-        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
-    }
-	return 0;
-}
-  // } Driver Code Ends
